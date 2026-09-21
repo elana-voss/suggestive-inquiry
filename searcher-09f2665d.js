9416c40e29ce755644e3ac7b1fc85dd48de3fb94
elana-voss <elana-voss@users.noreply.github.com>
2026-09-21 15:20:48 +0000

    deploy: 33296f413994073408b4b639ee811ca326fec7a4

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-1b3f5bba.js',
+                path_to_root + 'searchindex-488598dc.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
